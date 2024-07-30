2dd68859ca27cfca33009a79f3c07dbbd1b9192c
tkorza <tkorza@inbox.ru>
2024-07-30 23:48:14 +0300

    if else hw

---
+++ 12. if else/11/app.js
@@ -0,0 +1,15 @@
+/*
+ Напишите программу, в которой вычисляется сумма, разность и произведение
+двух целых чисел, введенных с клавиатуры.
+ */
+let a = +prompt()
+let b = +prompt()
+
+
+console.log("сумма");
+console.log(a + b);
+console.log("разность");
+console.log(a - b);
+console.log("произведение");
+console.log(a * b);
+
---
+++ 12. if else/14/app.js
@@ -0,0 +1,16 @@
+/*
+. Вы вводите значение с клавиатуры. Если значение четное, то вывести true, в
+противном случае false
+ */
+let a = prompt()
+
+if (a == 0) {
+    console.log(false);
+}
+else if (a % 2 == 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+
